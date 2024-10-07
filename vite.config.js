0d5aa649ad7233080dd2fbb301f4b3bb4b29d480
GMGonzaloMata <gonza.mata2003@gmail.com>
2024-10-07 01:19:29 -0300

    Implementación a PWA

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Mi Aplicación PWA',
+        short_name: 'MiApp',
+        description: 'Una descripción de la app PWA',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   server: {
-    historyApiFallback: true,
+    historyApiFallback: true, // Para manejar rutas en react-router
   },
   build: {
-    outDir: 'dist'
-  }
+    outDir: 'dist', // Directorio de salida para el build
+  },
 })
