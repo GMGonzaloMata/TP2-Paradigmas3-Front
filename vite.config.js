65799dec2f68e93aa07af0e78b0daf25e7373b1e
GMGonzaloMata <gonza.mata2003@gmail.com>
2024-10-07 00:28:50 -0300

    Borrar cambios

--- vite.config.js
+++ vite.config.js
@@ -4,22 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://tp2-paradigmas3-back.onrender.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '') // Redirige las solicitudes de /api a tu backend
-      }
-    },
-    port: 3000, // Puerto en el que corre tu frontend localmente
-  },
-  build: {
-    outDir: 'dist', // Carpeta de salida para el build
-  },
-  resolve: {
-    alias: {
-      '@': '/src', // Alias para importaciones
-    },
-  }
 })
